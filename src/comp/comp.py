ef9d49b8e84ca34c77f9598917bf132a0725722b
Kate McGee <mcgeehee@gmail.com>
2020-08-07 12:26:11 -0400

    almost finished with comp file

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,5 +1,7 @@
+import string
 # The following list comprehension exercises will make use of the 
 # defined Human class. 
+
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -25,43 +27,45 @@ humans = [
 # whose name starts with 'D':
 print("Starts with D:")
 a = []
+a = [i for i in humans if i.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+b = [i for i in humans if i.name[-1] == 'e']
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [i for i in humans if i.name[0] == 'C' or i.name[0] == 'D' or i.name[0] == 'E' or i.name[0] == 'F' or i.name[0] == 'G']
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [i.age*10 for i in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{i.name}-{i.age}" for i in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [i for i in humans if i.age in range(27,33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [f"{i.name.upper()}-{i.age+5}" for i in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
